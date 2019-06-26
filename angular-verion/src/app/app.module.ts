8d0a59d083fa85a8bebd7df746f25c0eae951fb8
shahzad <mshahzad@omniumgroup.com>
2019-06-26 23:09:34 +0200

    added new components

--- angular-verion/src/app/app.module.ts
+++ angular-verion/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './app/resume/resume.component';
+import { WorkComponent } from './app/work/work.component';
+import { ContactComponent } from './app/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
